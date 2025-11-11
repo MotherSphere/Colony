a4c342478f047f8b9bfe654fe084b08ac6eea33a
Kaanta <107195318+Kaanta0@users.noreply.github.com>
2025-11-11 20:09:00 +0100

    Revert "Adjust language card rounded corners"

--- src/utils/drawing.hpp
+++ src/utils/drawing.hpp
@@ -5,18 +5,8 @@
 namespace colony::drawing
 {
 
-enum RoundedCorner
-{
-    CornerNone = 0,
-    CornerTopLeft = 1 << 0,
-    CornerTopRight = 1 << 1,
-    CornerBottomLeft = 1 << 2,
-    CornerBottomRight = 1 << 3,
-    CornerAll = CornerTopLeft | CornerTopRight | CornerBottomLeft | CornerBottomRight
-};
+void RenderFilledRoundedRect(SDL_Renderer* renderer, const SDL_Rect& rect, int radius);
 
-void RenderFilledRoundedRect(SDL_Renderer* renderer, const SDL_Rect& rect, int radius, int cornerMask = CornerAll);
-
-void RenderRoundedRect(SDL_Renderer* renderer, const SDL_Rect& rect, int radius, int cornerMask = CornerAll);
+void RenderRoundedRect(SDL_Renderer* renderer, const SDL_Rect& rect, int radius);
 
 } // namespace colony::drawing

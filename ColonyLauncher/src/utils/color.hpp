d37221ad2e7a91d9dc2742d7db0440239148929c
MotherSphere <linhajahad@gmail.com>
2025-11-30 16:34:54 +0100

    Fix build issues and add color hex utility

--- ColonyLauncher/src/utils/color.hpp
+++ ColonyLauncher/src/utils/color.hpp
@@ -2,6 +2,7 @@
 
 #include <SDL2/SDL.h>
 
+#include <string>
 #include <string_view>
 
 namespace colony::color
@@ -12,5 +13,7 @@ SDL_Color ParseHexColor(std::string_view hex, SDL_Color fallback = {255, 255, 25
 SDL_Color Mix(const SDL_Color& a, const SDL_Color& b, float t);
 
 void RenderVerticalGradient(SDL_Renderer* renderer, const SDL_Rect& area, SDL_Color top, SDL_Color bottom);
+
+std::string ToHexString(const SDL_Color& color);
 
 } // namespace colony::color
